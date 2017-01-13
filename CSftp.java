7ead74d1f792a414c86b75360215401b9076c7d6
CS 317 course account <cs317@ugrad.cs.ubc.ca>
2017-01-13 11:04:35 -0800

    Initial files

---
+++ CSftp.java
@@ -0,0 +1,42 @@
+
+import java.lang.System;
+import java.io.IOException;
+
+//
+// This is an implementation of a simplified version of a command 
+// line ftp client. The program always takes two arguments
+//
+
+
+public class CSftp
+{
+    static final int MAX_LEN = 255;
+    static final int ARG_CNT = 2;
+
+    public static void main(String [] args)
+    {
+	byte cmdString[] = new byte[MAX_LEN];
+
+	// Get command line arguments and connected to FTP
+	// If the arguments are invalid or there aren't enough of them
+        // then exit.
+
+	if (args.length != ARG_CNT) {
+	    System.out.print("Usage: cmd ServerAddress ServerPort\n");
+	    return;
+	}
+
+	try {
+	    for (int len = 1; len > 0;) {
+		System.out.print("csftp> ");
+		len = System.in.read(cmdString);
+		if (len <= 0) 
+		    break;
+		// Start processing the command here.
+		System.out.println("900 Invalid command.");
+	    }
+	} catch (IOException exception) {
+	    System.err.println("998 Input error while reading commands, terminating.");
+	}
+    }
+}
