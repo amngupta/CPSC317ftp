def3c08f1894b408a4b42dec7c7bf986a7b2e5bb
amngupta <amangupta96@outlook.com>
2017-01-17 17:23:41 -0800

    Added code to establish connection to server

--- src/Connector.java
+++ src/Connector.java
@@ -1,6 +1,13 @@
 /**
  * Created by Aman Gupta on 1/17/17.
  */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
 public class Connector {
 
     public static void main(String args[]){
@@ -12,8 +19,34 @@ public class Connector {
             int Port = 21;
             if(args.length == 2){
                 Port = Integer.parseInt(args[1]);
+                while (true) {
+                    try{
+                        Socket socket = new Socket(IPAddress, Port);
+                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                        System.out.println("server says:" + br.readLine());
+
+                        BufferedReader userInputBR = new BufferedReader(new InputStreamReader(System.in));
+
+                        String userInput = userInputBR.readLine();
+//                        System.out.println("csftp> ");
+                        out.println(userInput);
+
+                        System.out.println("server says:" + br.readLine());
+
+                        if ("quit".equalsIgnoreCase(userInput)) {
+                            socket.close();
+                            break;
+                        }
+                    }
+                    catch(Exception e){
+                        System.out.println(e);
+                        break;
+                    }
+                }
             }
-            System.out.println(IPAddress);
+            System.out.println(IPAddress + Port);
         }
     }
 }
