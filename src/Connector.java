426eb29861c63d7311247a9dce375e86f3ad4dda
amngupta <amangupta96@outlook.com>
2017-01-18 18:14:38 -0800

    Moved code that runs after connection established to a function - Added FTP codes as comments - Fix so main method does not run infinitely

--- src/Connector.java
+++ src/Connector.java
@@ -7,8 +7,39 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-
+/*
+* Error Codes:
+* 220: Connection Successful
+* 530: Need to login
+* 331: Need password
+* 230: Login Successful
+* 550: Failed to open file
+* 250: Directory successfully changed
+* 227: Entering Passive Mode
+* */
 public class Connector {
+
+    static void runClient(BufferedReader br, Socket socket, PrintWriter out){
+        System.out.println("Running client");
+        while(true) {
+            try {
+                BufferedReader userInputBR = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("csftp> ");
+                String userInput = userInputBR.readLine();
+                out.println(userInput);
+
+                System.out.println("server says:" + br.readLine());
+                if ("quit".equalsIgnoreCase(userInput)) {
+                    out.println("QUIT");
+                    socket.close();
+                    return;
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+                break;
+            }
+        }
+    }
 
     public static void main(String args[]){
         if(args.length < 1){
@@ -19,33 +50,24 @@ public class Connector {
             int Port = 21;
             if(args.length == 2){
                 Port = Integer.parseInt(args[1]);
-                while (true) {
-                    try{
-                        Socket socket = new Socket(IPAddress, Port);
-                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-                        System.out.println("server says:" + br.readLine());
-
-                        BufferedReader userInputBR = new BufferedReader(new InputStreamReader(System.in));
-
-                        String userInput = userInputBR.readLine();
-//                        System.out.println("csftp> ");
-                        out.println(userInput);
-
-                        System.out.println("server says:" + br.readLine());
-
-                        if ("quit".equalsIgnoreCase(userInput)) {
-                            socket.close();
-                            break;
-                        }
-                    }
-                    catch(Exception e){
-                        System.out.println(e);
-                        break;
-                    }
-                }
             }
+//            while (true) {
+                try{
+                    Socket socket = new Socket(IPAddress, Port);
+                    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                    System.out.println(br.readLine());
+//                    if((br.readLine()).contains("220")) {
+                        runClient(br, socket, out);
+//                        break;
+//                    }
+                }
+                catch(Exception e){
+                    System.out.println(e);
+//                    break;
+                }
+//            }
             System.out.println(IPAddress + Port);
         }
     }
